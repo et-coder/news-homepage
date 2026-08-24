f0e747de92c8113a8b904970200f38d9d167c02b
Nati <thecodegeek1@gmail.com>
2026-08-24 11:59:53 +0300

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+const navItems = ["Home", "New", "Popular", "Trending", "Categories"];
+
+const asides = [
+  {
+    heading: " Hydrogen VS Electric Cars ",
+    description: "Will hydrogen-fueled cars ever catch up to EVs?",
+  },
+  {
+    heading: "The Downsides of AI Artistry",
+    description:
+      "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    heading: "Is VC Funding Drying Up?",
+    description:
+      "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+  },
+];
+
+export { navItems, asides };
