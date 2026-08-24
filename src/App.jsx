bd59c7a6ad58e4515ddb2c38c8a0845c26843a9f
Nati <thecodegeek1@gmail.com>
2026-08-24 11:59:06 +0300

    add custom styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Aside from "./components/Aside";
+import Related from "./components/Related";
 
-const App = () => <></>;
+const App = () => (
+  <>
+    <div className="max-w-[1140px] mx-auto px-4 py-7 font-Inter">
+      <Header />
+      <div className="grid grid-cols-1">
+        <Main />
+        <Aside />
+        <Related />
+      </div>
+    </div>
+  </>
+);
 export default App;
